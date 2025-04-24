6501a8c450f8b95d0c21149e03db6b3cdd9cace8
literalnansensu <howtueatlife@gmail.com>
2025-04-24 21:34:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🎈 Anjay mabar")
 st.write(
-    "aku cinta sosiologi, aku cinta wow spaghetti, indomie seblak hot jeletot, dan tori miso, aku cinta Eve, aku cinta semua lagu yg mengandung unsur blue, aku cinta fanart beserta artists-nya, aku cinta greenhouse effect, aku cinta Kita Ikuyo (disclaimer gw bukan solo stan kb), aku cinta Anaxagores, aku cinta Adventurine, aku cinta tahu Sumedang, aku cinta kucing pusdai, aku cinta Somos Libres by Sore, aku cinta"
+    "aku cinta sosiologi, aku cinta wow spaghetti, indomie seblak hot jeletot, dan tori miso, aku cinta Eve, aku cinta semua lagu yg mengandung unsur blue, aku cinta fanart beserta artists-nya, aku cinta greenhouse effect, aku cinta Kita Ikuyo (disclaimer gw bukan solo stan kb), aku cinta Anaxagores, aku cinta Adventurine, aku cinta tahu Sumedang, aku cinta kucing pusdai, aku cinta Somos Libres by Sore, aku cinta Maudi dan cowok berjaket VIII Fearless yg ada dibawah (pov Ardi inimah)"
 )
 st.image("view/54d12abd-0cf9-4653-8f5f-6027cd197aeb.jpeg")
 st.image("view/IMG_2834.jpeg")
