5ab1ed5b9813c8747824493bcb1e7ff886c68c2a
literalnansensu <howtueatlife@gmail.com>
2025-04-24 19:47:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Anjay mabar")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "aku cinta sosiologi, aku cinta wow spaghetti, indomie seblak hot jeletot, dan tori miso, aku cinta Eve, aku cinta semua lagu yg mengandung unsur blue, aku cinta greenhouse effect, aku cinta Kita Ikuyo (disclaimer gw bukan solo stan kb), aku cinta Anaxagores, aku cinta Adventurine, aku cinta tahu Sumedang, aku cinta kucing pusdai, aku cinta Somos Libres by Sore, aku cinta"
 )
+st.image("view/54d12abd-0cf9-4653-8f5f-6027cd197aeb.jpeg")
